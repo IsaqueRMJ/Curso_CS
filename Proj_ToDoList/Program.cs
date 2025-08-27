c77bf19742f5e7159606f786448372baf33d46d5
Isaque <franciscoisaquemagalhaes@gmail.com>
2025-08-27 11:28:30 -0300

    Removido arquivos velhos e iniciado projeto Lista Tarefas

---
+++ Proj_ToDoList/Program.cs
@@ -0,0 +1,57 @@
+﻿List<string> tarefas = new List<string>() { };
+var running = true;
+while (running)
+{
+    Console.WriteLine("\nBem vindo(a) a sua lisa de tarefas");
+    Console.Write("1-Adicionar tarefa\n2-Listar terefas\n3-Remover tarefa\n4-Sair\n\n");
+    var escolha = Console.ReadLine();
+    switch (escolha)
+    {
+        case "1":
+            Console.WriteLine($"Digite a tarefa que você deseja adcionar: ");
+            var tarefa = Console.ReadLine();
+            tarefas.Add(tarefa);
+            Console.Write("Sua tarefa foi adicionada...");
+            Console.Read();
+            continue;
+        case "2":
+            if (tarefas.Count() == 0)
+            {
+                Console.Write("Atualmente sua lista de tarefas está vazia... ");
+                Console.Read();
+            }
+            else if (tarefas.Count() > 0)
+            {
+                Console.WriteLine("Você tem tarefas na sua lista: ");
+                int i = 0;
+                foreach (var Tarefa in tarefas)
+                {
+                    Console.WriteLine($"{i+1}-{tarefas[i]}");
+                    i++;
+                }
+                Console.Read();
+            }
+            continue;
+        case "3":
+            Console.WriteLine("Você deseja remover a tarefa por nome ou por indice? ");
+            var escolha_01 = Console.ReadLine();
+            if (escolha.ToLower() == "nome")
+            {
+                
+            }if (escolha.ToLower() == "indice")
+            {
+                
+            }
+            continue;
+        case "4":
+            Console.WriteLine("Saindo...");
+            Console.Read();
+            break;
+        default:
+            Console.WriteLine("Valor inválido, tente novamente...");
+            Console.Read();
+            continue;
+        
+    }
+    break;
+}
--- proj_LojaSapatos/Program.cs
+++ proj_LojaSapatos/Program.cs
@@ -10,16 +10,17 @@
     }
     else if (entradaCompra == "s")
     {
-        Console.WriteLine("Digite o tamanho, a cor, a marca, o preço e se vai ter cadarço ou não(true e false)\n(Nessa ordem e com espaço entre as informações)");
+        Console.WriteLine("Digite o tamanho, o tipo(tênis...), a cor, a marca, o preço e se vai ter cadarço ou não(true e false)\n(Nessa ordem e com espaço entre as informações)");
         var entradaSapato = Console.ReadLine();
         string[] infos = entradaSapato.Split();
         var tamanhoEntrada = Convert.ToInt32(infos[0]);
-        var corEntrada = infos[1];
-        var marcaEntrada = infos[2];
-        var precoEntrada = Convert.ToDecimal(infos[3]);
-        var cadarcoEntrada = Convert.ToBoolean(infos[4]);
+        var tipoEntrada = infos[1];
+        var corEntrada = infos[2];
+        var marcaEntrada = infos[3];
+        var precoEntrada = Convert.ToDecimal(infos[4]);
+        var cadarcoEntrada = Convert.ToBoolean(infos[5]);
 
-        Sapato sapato_01 = new(tamanhoEntrada, precoEntrada, corEntrada, marcaEntrada, cadarcoEntrada);
+        Sapato sapato_01 = new(tamanhoEntrada, precoEntrada, tipoEntrada,corEntrada, marcaEntrada, cadarcoEntrada);
         sapato_01.sobre();
         Console.Read();
     }
@@ -33,47 +34,28 @@
 }
 public class Sapato
 {
+    public string tipo;
     public decimal preco;
     public string cor;
     public int tamanho;
-    public string marca;
+    public string[] marca = ["Adidas","Nike","Puma","New Balance"];
     public bool cadarco;
-    public Sapato(int Tamanho) => tamanho = Tamanho;
+    public Sapato(){}
 
-    public Sapato(int Tamanho, decimal Preco, string Cor, string Marca, bool Cadarco) : this(Tamanho)
+    public Sapato(int Tamanho, decimal Preco, string Tipo, string Cor, string Marca, bool Cadarco)
     {
+        tamanho = Tamanho;
         preco = Preco;
+        tipo = Tipo;
         cor = Cor;
-        marca = Marca;
         cadarco = Cadarco;
 
     }
-    public void sobre() => Console.WriteLine($"Seu sapato é do tamanho {tamanho}, da cor {cor}, da marca {marca}, custa {preco} e ele" +(cadarco ? "tem cadarço":"Não tem cadarço"));
-    public void amarrar(Sapato sapato)
-    {
-        var amarrado = false;
-        while (true)
-        {
-            if (cadarco == false)
-            {
-                Console.WriteLine("Seu sapato nem tem cadarço, não da para amarrar");
-                Console.Read();
-                break;
-            }
-            else if (amarrado == true)
-            {
-                Console.WriteLine("O cadarço está amarrado");
-                Console.Read();
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Opa seu cadarço está dessamarrado, melhor amarrar");
-                Console.Read();
-                amarrado = true;
-                continue;
-            }
-        }
-    }
+    public void sobre() => Console.WriteLine($"Seu sapato é do tamanho {tamanho}, da cor {cor}, da marca {marca}, custa {preco} e ele" + (cadarco ? "tem cadarço" : "Não tem cadarço"));
 
+}
+
+public class Loja:Sapato
+{
+    
 }
