60f422825cc274f1318ed0af9672b110467852c3
Isaque <franciscoisaquemagalhaes@gmail.com>
2025-08-31 22:02:27 -0300

    Projeto ToDoList atualizado

--- Proj_ToDoList/Program.cs
+++ Proj_ToDoList/Program.cs
@@ -1,13 +1,12 @@
 ﻿List<string> tarefas = new List<string>() { };
-var running = true;
-while (running)
+while (true)
 {
     Console.WriteLine("\nBem vindo(a) a sua lista de tarefas");
     Console.Write("1-Adicionar tarefa\n2-Listar terefas\n3-Remover tarefa\n4-Sair\n\n");
     var escolha = Console.ReadLine();
     switch (escolha)
     {
-        case "1":
+        case "1"://Adicionar itens
             while (true)
             {
                 Console.WriteLine($"Digite a tarefa que você deseja adcionar: ");
@@ -16,12 +15,12 @@ while (running)
                 Console.Write("Sua tarefa foi adicionada...\n");
 
                 Console.Write("Você deseja continuar a adicionar (s/n)? ");
-                var continuar = Console.ReadLine();
-                if (continuar == "s")
+                escolha = Console.ReadLine();
+                if (escolha == "s")
                 {
                     continue;
                 }
-                else if (continuar == "n")
+                else if (escolha == "n")
                 {
                     Console.Write("Click qualquer tecla para volar ao menu...");
                     Console.Read();
@@ -35,54 +34,89 @@ while (running)
                 }
             }
             continue;
-        case "2":
-            if (tarefas.Count() == 0)
+        case "2"://Listar itens
+            if (tarefas.Count() == 0)//Confere se tem algo na lista
             {
                 Console.Write("Atualmente sua lista de tarefas está vazia... ");
                 Console.Read();
             }
-            else if (tarefas.Count() > 0)
+            else if (tarefas.Count() > 0)//Se tiver algo ele mostra os itens
             {
                 Console.WriteLine("Você tem tarefas na sua lista: ");
                 int i = 0;
-                foreach (var Tarefa in tarefas)
+                foreach (var Tarefa in tarefas)//Para cada item na lista ele repete
                 {
-                    Console.WriteLine($"{i+1}-{tarefas[i]}");
+                    Console.WriteLine($"{i + 1}-{tarefas[i]}");
                     i++;
                 }
                 Console.Read();
             }
             continue;
-        case "3":
-            Console.WriteLine("Você deseja remover a tarefa por nome ou por indice? ");
-            var escolha_01 = Console.ReadLine();
-            if (escolha_01.ToLower() == "nome")
+        case "3"://Remover tarefa
+            if (tarefas.Count() == 0)//Verificar se tem algo na lista
             {
-                Console.WriteLine("Digite a tarefa");
-                var tarefaRemove = Console.ReadLine();
-                tarefas.Remove(tarefaRemove);
+                Console.Write("Atualmente sua lista de tarefas está vazia... ");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Você deseja remover a tarefa por nome ou por indice? ");//Remover de acordo com a listagem ou pelo a escrita da tarefa
+                escolha = Console.ReadLine();
+                if (escolha.ToLower() == "nome")//Por escrito
+                {
+                    Console.WriteLine("Digite a tarefa(escreva por extenso)");
+                    var tarefaRemove = Console.ReadLine();
+                    if (tarefas.IndexOf(tarefaRemove) == -1)//Se não tiver ele avisa que não existe essa tarefa
+                    {
+                        Console.WriteLine("Esse item não está na lista...");
+                        Console.Read();
+                    }
+                    else
+                    {
+                        tarefas.Remove(tarefaRemove);
+                        Console.Write("Removido com sucesso...");
+                        Console.Read();
+                    }
+                        
 
-                Console.Write("Removido com sucesso...");
-                Console.Read();
+                    
+                }
+                else if (escolha.ToLower() == "indice")//Por numero
+                {
+                    Console.WriteLine("Digite o indice da tarefa(valor numérico)");
+                    var tarefaRemove = Convert.ToInt32(Console.ReadLine());
+                    if (tarefaRemove > tarefas.Count() || tarefaRemove < 0)
+                    {
+                        Console.WriteLine("Esse item não está na lista...");
+                        Console.Read();
+                    }
+                    else
+                    {
+                        tarefas.RemoveAt(tarefaRemove - 1);
+                        Console.Write("Removido com sucesso...");
+                        Console.Read(); 
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("ERROR, opção inexistente, retornando ao menu...");
+                    Console.Read(); 
+                }
+                    
+                
             }
-            if (escolha_01.ToLower() == "indice")
-            {
-                Console.WriteLine("Digite o indice da tarefa");
-                var tarefaRemove = Convert.ToInt32(Console.ReadLine());
-                tarefas.RemoveAt(tarefaRemove-1);
-                Console.Write("Removido com sucesso...");
-                Console.Read();
-            }
+
             continue;
-        case "4":
+        case "4"://Sair
             Console.WriteLine("Saindo...");
             Console.Read();
             break;
-        default:
+        default://Digitar um valor não existente
             Console.WriteLine("Valor inválido, tente novamente...");
             Console.Read();
             continue;
-        
+
     }
     break;
 }
